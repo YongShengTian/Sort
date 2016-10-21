796b7747967db56baf49ac811da3f935c19c15cf
tys <tys@tys-PC>
2016-10-21 22:46:41 +0800

    完善希尔

--- 排序算法/src/com/Sort/希尔排序.java
+++ 排序算法/src/com/Sort/希尔排序.java
@@ -3,5 +3,24 @@ package com.Sort;
 public class Ï£¶ûÅÅÐò {
 	public static void main(String[] args) {
 		int[] a = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1 };
+		xiersort(a);
+		for (int i = 0; i < a.length; i++) {
+			int j = a[i];
+			System.out.println(j);
+		}
+	}
+	private static void xiersort(int[] a) {
+		int d=a.length;
+		while (d>1) {
+			for (int i = d; i <a.length; i++) {
+				int currentvalue=a[i];
+				int j;
+				for (j = i - d; j >= 0 && a[j] > currentvalue; j-=d) {
+					a[j + d] = a[j];
+				}
+				a[j + d] = currentvalue;
+			}
+			d=d/2;
+		}
 	}
 }
